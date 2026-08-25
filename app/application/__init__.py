f1545540b571a262ad1e59cd45e5cb4796e6141e
tchi99 <93987783+tchi99@users.noreply.github.com>
2026-08-25 07:24:05 -0400

    #126: exposer le planning read repository port

--- app/application/__init__.py
+++ app/application/__init__.py
@@ -8,7 +8,11 @@ transport and persistence technologies must not leak into this package.
 from .allocation_service import AllocationService
 from .planning_service import PlanningService
 from .read_models import DemandReadModel, SegmentReadModel
-from .repository_ports import DemandRepositoryPort, SegmentRepositoryPort
+from .repository_ports import (
+    DemandRepositoryPort,
+    PlanningReadRepositoryPort,
+    SegmentRepositoryPort,
+)
 
 __all__ = [
     "AllocationService",
@@ -16,5 +20,6 @@ __all__ = [
     "DemandReadModel",
     "SegmentReadModel",
     "DemandRepositoryPort",
+    "PlanningReadRepositoryPort",
     "SegmentRepositoryPort",
 ]
