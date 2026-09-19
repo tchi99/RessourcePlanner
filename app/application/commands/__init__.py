3db2552cec87d5f17cc783da7cb6c80a6f33f667
tchi99 <93987783+tchi99@users.noreply.github.com>
2026-09-19 18:31:23 -0400

    #288C Export DemandLineInput

--- app/application/commands/__init__.py
+++ app/application/commands/__init__.py
@@ -11,6 +11,7 @@ from .demand import (
     DemandCancelCommand,
     DemandCorrectionCommand,
     DemandCreateCommand,
+    DemandLineInput,
     DemandSubmitCommand,
     DemandUpdateCommand,
 )
@@ -30,6 +31,7 @@ __all__ = [
     "DemandCancelCommand",
     "DemandCorrectionCommand",
     "DemandCreateCommand",
+    "DemandLineInput",
     "DemandPeriodInput",
     "DemandPeriodsReplaceCommand",
     "DemandSubmitCommand",
